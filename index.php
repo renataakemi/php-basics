67b0397058a927a34592e001ddb0b352a98197d0
Renata Akemi <renatakemi@hotmail.com>
2015-06-13 14:30:13 +0000

    Added link

--- index.php
+++ index.php
@@ -1,14 +1,27 @@
+<?php $title = 'PHP Projects' ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
 
 <meta charset="UTF-8">
-<title>PHP Basics</title>
+<title><?= $title ?></title>
 
 </head>
 <body>
 
-<h1>PHP Basics</h1>
+<h1><?= $title ?></h1>
+<ul>
+  <?php
+      $dir = new DirectoryIterator('.');
+      $ignoredList = array('.', '..', 'css', '.c9', '.git');
+      
+      foreach($dir as $fileInfo) {
+        if($fileInfo->isDir() && !in_array($fileInfo->getfilename(), $ignoredList)) {
+          echo "<li><a href=\"{$fileInfo->getfilename()}\">{$fileInfo->getfilename()}</a></li>";  
+        }
+      }
+  ?>
+</ul>
 
 </body>
 </html>
