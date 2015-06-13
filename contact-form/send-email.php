f9d2060c31fbb906f216bba4ea0b25080850f067
Edy Segura <edysegura@gmail.com>
2015-06-13 09:34:26 -0300

    Update send-email.php
    Remove php code

--- contact-form/send-email.php
+++ contact-form/send-email.php
@@ -12,24 +12,5 @@
 
 <h1>Sent Data</h1>
 
-<p>
-	Data
-	<br>
-	<?= $_POST['name'] ?>
-</p>
-
-<p>
-	Data
-	<br>
-	<?= $_POST['email'] ?>
-</p>
-
-<p>
-	Data
-	<pre><?= $_POST['message'] ?></pre>
-</p>
-
-<?php var_dump( mail("edysegura@gmail.com", 'Test', 'Hi there! This is a test!') ); ?>
-
 </body>
 </html>
